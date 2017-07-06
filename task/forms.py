659332925823e2c104ac2fb738f732a191b7a81a
AdamovskiyAnatoliy <adamovskiy.job@gmail.com>
2017-07-06 22:40:27 +0300

    Add edit

--- task/forms.py
+++ task/forms.py
@@ -1,3 +1,8 @@
 from django import forms
 
 from .models import Note
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Note
+        fields = ('must_complete_before','task')
--- task/views.py
+++ task/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 
 from .models import Note
+from .forms import TaskForm
 
 
 def task_list(request):
@@ -10,6 +12,20 @@ def task_list(request):
 def task_detail(request, pk):
     task = get_object_or_404(Note, pk=pk)
     return render(request, 'task/task_detail.html', {'task': task})
+
+def task_edit(request, pk):
+    task = get_object_or_404(Note, pk=pk)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.athor = request.user
+            task.create_date = timezone.now()
+            task.save()
+            return redirect('task_detail', pk=task.pk)
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'task/task_edit.html', {'form': form})
 
 def task_complete_list(request):
     tasks = Note.objects.filter(complete_value=True)
