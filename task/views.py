cacd65d20f3cbd861c1c3189e5b28c0032767940
AdamovskiyAnatoliy <adamovskiy.job@gmail.com>
2017-07-07 02:45:28 +0300

    Add new task

--- task/views.py
+++ task/views.py
@@ -13,13 +13,28 @@ def task_detail(request, pk):
     task = get_object_or_404(Note, pk=pk)
     return render(request, 'task/task_detail.html', {'task': task})
 
+def task_new(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.author = request.user
+            task.create_date = timezone.now()
+            task.save()
+            return redirect('task_detail', pk=task.pk)
+    else:
+        form = TaskForm()
+    return render(request, 'task/task_edit.html', {'form': form})
+
+
+
 def task_edit(request, pk):
     task = get_object_or_404(Note, pk=pk)
     if request.method == "POST":
         form = TaskForm(request.POST, instance=task)
         if form.is_valid():
             task = form.save(commit=False)
-            task.athor = request.user
+            task.author = request.user
             task.create_date = timezone.now()
             task.save()
             return redirect('task_detail', pk=task.pk)
