df07fe0cb28766c5fc3fb47d1f0bba4be8e04b7c
AdamovskiyAnatoliy <adamovskiy.job@gmail.com>
2017-07-06 14:45:45 +0300

    Add task detals

--- timemanager/settings.py
+++ timemanager/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'timemanager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,6 +68,8 @@ TEMPLATES = [
         },
     },
 ]
+
+
 
 WSGI_APPLICATION = 'timemanager.wsgi.application'
 
