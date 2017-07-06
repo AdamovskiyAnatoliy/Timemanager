4fab3eb0435e0bb6be2b530a1c97630aca6ad862
AdamovskiyAnatoliy <adamovskiy.job@gmail.com>
2017-07-06 19:26:50 +0300

    Mo bugs and start app login and register

--- task/urls.py
+++ task/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^$', views.task_list, name='task_list'),
+    url(r'^$', views.main, name='main'),
+    url(r'^task_list/$', views.task_list, name='task_list'),
 
     #url(r'^task/complete$', views.task_complete, name='task_complete').
-    url(r'^task/(?P<pk>[0-9]+)/$', views.task_detail, name='task_detail'),
-    url(r'^task_complete_list/$', views.task_complete_list,name='task_complete_list')
     #url(r'^task/new/$', views.task_new, name='task_new'),
     #url(r'^task/(?P<pk>[0-9]+)/edit/$', views.task_edit, name='task_edit'),
+    url(r'^task/(?P<pk>[0-9]+)/$', views.task_detail, name='task_detail'),
+    url(r'^task_complete_list/$', views.task_complete_list, name='task_complete_list'),
+    url(r'^register/$', views.RegistrationView, name='register'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+
+
 ]
--- task/views.py
+++ task/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import CreateView
 
 from .models import Note
+from .forms import UserRegistrationForm
 
+
+class RegistrationView(CreateView):
+    """ Custom view for registering students """
+    form_class = UserRegistrationForm
+    template_name = 'registration/register.html'
+    success_url = 'main'
+
+    def form_valid(self, form):
+        user = form['user'].save()
+        student = form['student'].save(commit=False)
+        student.user = User.objects.get(username=user.username)
+        student.save()
+        new_user = authenticate(username=form['user'].cleaned_data['username'],
+                                password=form['user'].cleaned_data['password1'])
+        login(self.request, new_user)
+        return redirect(reverse(self.success_url))
+
+
+
+
+def main(request):
+    return render(request, 'main.html', {})
 
 def task_list(request):
     tasks = Note.objects.filter(complete_value=False)
