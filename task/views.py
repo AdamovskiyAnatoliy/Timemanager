79b67d5e47dfe2e5016ba8f523347490786f5dd2
AdamovskiyAnatoliy <adamovskiy.job@gmail.com>
2017-07-06 03:28:03 +0300

    Add templates and app task/views.py

--- task/views.py
+++ task/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from .models import Note
+
+
+def task_list(request):
+    tasks = Note.objects.filter(complete_value=False)
+    return render(request, 'task/task_list.html', {'tasks': tasks})
+
+
 # Create your views here.
