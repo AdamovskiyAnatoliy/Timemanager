08eed877e84e685966a7960c748e9f959605ad52
AdamovskiyAnatoliy <adamovskiy.job@gmail.com>
2017-07-07 21:19:52 +0300

    Add singup

--- task/urls.py
+++ task/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
 
+
 urlpatterns = [
     url(r'^$', views.task_list, name='task_list'),
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
@@ -17,5 +18,5 @@ urlpatterns = [
     #url(r'^task/(?P<pk>[0-9]+)/edit/$', views.task_edit, name='task_edit'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
-
+    url(r'^signup/$', views.signup, name='signup'),
 ]
--- task/views.py
+++ task/views.py
@@ -5,6 +5,23 @@ from .models import Note
 from .forms import TaskForm
 
 
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
 def task_list(request):
     tasks = Note.objects.filter(complete_value=False)
     return render(request, 'task/task_list.html', {'tasks': tasks})
