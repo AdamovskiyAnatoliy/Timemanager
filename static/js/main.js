84f3f54476c8ffb3aaa04f8790da5fb1e850ef32
AdamovskiyAnatoliy <adamovskiy.job@gmail.com>
2017-07-10 14:46:34 +0300

    Good timer

--- static/js/main.js
+++ static/js/main.js
@@ -1,22 +1,5 @@
 function timer(a, b){
-    var v = a;
-    if (v.slice(-4,-1) == 'a.m'){
-      if (v.slice(-10,-9) == '1'){
-        v = v.slice(0,-12) + ' '+ v.slice(-10,-5) + ':00' ;
-      }
-      else{
-        v = v.slice(0,-11) + ' ' + v.slice(-9,-5) + ':00' ;
-      }
-    }
-    else{
-      if (v.slice(-10,-9) == '1'){
-        v = v.slice(0,-12) + ' '+ (Number(v.slice(-10,-8))+12 )+v.slice(-8,-5) + ':00' ;
-      }
-      else{
-        v = v.slice(0,-11) + ' '+ (Number(v.slice(-9,-8))+12 )+v.slice(-8,-5) + ':00' ;
-      }
-    }
-    var countDownDate = new Date(v).getTime();
+    var countDownDate = new Date(a).getTime();
     var x = setInterval(function() {
       var now = new Date().getTime();
       var distance = countDownDate - now;
