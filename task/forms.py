c342abf16ba196798c08fc5b69ad48c12b69a918
AdamovskiyAnatoliy <adamovskiy.job@gmail.com>
2017-07-16 04:18:14 +0300

    Fix Datetime

--- task/forms.py
+++ task/forms.py
@@ -1,10 +1,11 @@
 from django import forms
-from django.forms.widgets import DateTimeInput
+from django.forms.widgets import DateInput, TimeInput
 
 from .models import Note, Dream
 
 
 class TaskForm(forms.ModelForm):
+
     class Meta:
         model = Note
         fields = ('task', 'must_complete_before')
@@ -12,11 +13,12 @@ class TaskForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(TaskForm, self).__init__(*args, **kwargs)
 
-        self.fields['task'].widget.attrs['class'] = 'form-control '
-        self.fields['must_complete_before'].widget = DateTimeInput(attrs={
+        self.fields['task'].widget.attrs['class'] = 'form-control'
+        self.fields['must_complete_before'].widget = DateInput(attrs={
             'class':'vDateField', 'id':'id_must_complete_before_0',
             'name':'must_complete_before_0', 'size':'10', 'type':'text',
             })
+
 
 
 
